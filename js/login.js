999b4ff8419211c05cfc134b54b4c5eac2d6f1f6
Kmusale <kmusale@csu.fullerton.edu>
2018-03-17 20:03:46 -0700

    ..

--- js/login.js
+++ js/login.js
@@ -1,28 +1,27 @@
-$(document).ready(function() {
-  $('#login').click(function() {
+function loadAJAX() {
+  var request;
   var user = $('#uname').val();
   var password = $('#pwd').val();
-  var error = true;
 
-    $.ajax({
-      type: "POST",
-      url: "db1.json",
-      dataType: "json",
-      success: function(data) {
-        $each(data, function(key,value) {
-          if(user == students.fName && password == students.password) {
-        error = false;
-          }
-        });
-          if(error == false) {
-            document.location="save_ss.php?user" +user;
-          } else {
-            alert("Invalid Details");
-          }
+  if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+  } else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  request.open('GET', 'db1.json');
+  request.onreadystatechange = function() {
+    if ((request.readyState === 4) && (request.status === 200)) {
+      var items = JSON.parse(request.responseText);
+      for (var key in items) {
+        if ((user == items[key].fName) && (password == items[key].password)) {
+          alert("Valid User");
+          //window.location="welcome.html"
+        } else {
+         window.location="index.html";
         }
-        });
-          return false;
+      }
 
-
-  });
-});
+    }
+  }
+  request.send();
+}
